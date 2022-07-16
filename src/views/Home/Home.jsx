11e2d961957402e6dfecd8e13d0d685fc232d28a
Monica calle <monicacalle369@gmail.com>
2022-07-16 16:13:34 -0400

    feat: connect to api

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Card from "../../components/Card";
+import rick_morty from "../../images/rick_morty.jpg";
+import config from "../../config/constants";
 
-function Home() {
+const Home = () => {
+  const getCharacters = async () => {
+    const response = await fetch(`${config.ENDPOINT}/character`);
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
     <div>
-      <Card />
+      <Card img={rick_morty} text={"click for more"} action={getCharacters} />
     </div>
   );
-}
+};
 
 export default Home;
