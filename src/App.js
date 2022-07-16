638e4f2a10af7bf9b0f51f36859ec9e655bb5ed4
Monica calle <monicacalle369@gmail.com>
2022-07-16 14:39:35 -0400

    feat: change app bg color

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./views/Home/Home";
 
 function App() {
   return (
-    <div className="App d-flex flex-column min-vh-100">
+    <div className="App d-flex flex-column min-vh-100 bg-light">
       <Navbar />
       <Home />
       <Footer />
