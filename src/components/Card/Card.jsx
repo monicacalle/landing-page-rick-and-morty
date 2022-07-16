1a2fed5cb84dc573ce125d52c6a4e34953dcfde2
Monica calle <monicacalle369@gmail.com>
2022-07-16 14:23:43 -0400

    feat: create card component

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="card" style={{ width: "18rem" }}>
+      <img
+        src="https://images.unsplash.com/photo-1433086966358-54859d0ed716?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+        className="card-img-top"
+        alt="..."
+      />
+      <div className="card-body">
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
